936c1e1dc0c5f7b142815905acdcae494ac98d78
Jethro Carreon <shirocarreon@gmail.com>
2022-10-24 13:59:03 +0800

    initial commit

---
+++ primelands/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4)tt%eprwl_axut5=b0_ms5!2#d@$7w1+i-b6ffg)1nik2y45_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
